64a007b7bde297f95673e2cc999f826bea886f4e
Rutu Shah <shahrutu20@gmail.com>
2021-06-18 16:01:21 +0200

    initialising Webpack

--- src/client/index.js
+++ src/client/index.js
@@ -1,1 +1,13 @@
-alert('Hello! Welcome to travel app')
+// alert('Hello! Welcome to travel app')
+
+import {handleSubmit} from "./js/handleSubmit";
+import {plan_now} from "./js/app";
+import  {plan_trip} from "./js/handleSubmit";
+import  {remove_trip} from "./js/app";
+
+
+export {
+    handleSubmit,
+    plan_trip,
+    plan_now,
+    remove_trip}
